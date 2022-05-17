011f982638df39b3127a03231c11732a8217dd86
victor.hfigueiredo <victor.hfigueiredo@SBC0505290W10-1>
2022-05-17 16:01:58 -0300

    Adicionar arquivos de projeto.

---
+++ TrocaNomeArquivo/View/TelaPrincipal.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace TrocaNomeArquivo.View
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblPara = new System.Windows.Forms.Label();
+            this.tbxEndereco = new System.Windows.Forms.TextBox();
+            this.tbxDe = new System.Windows.Forms.TextBox();
+            this.tbxPara = new System.Windows.Forms.TextBox();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblEndereco.Location = new System.Drawing.Point(98, 76);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(68, 17);
+            this.lblEndereco.TabIndex = 0;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDe.Location = new System.Drawing.Point(98, 150);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(29, 17);
+            this.lblDe.TabIndex = 1;
+            this.lblDe.Text = "De:";
+            // 
+            // lblPara
+            // 
+            this.lblPara.AutoSize = true;
+            this.lblPara.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPara.Location = new System.Drawing.Point(98, 213);
+            this.lblPara.Name = "lblPara";
+            this.lblPara.Size = new System.Drawing.Size(39, 17);
+            this.lblPara.TabIndex = 2;
+            this.lblPara.Text = "Para:";
+            // 
+            // tbxEndereco
+            // 
+            this.tbxEndereco.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbxEndereco.Location = new System.Drawing.Point(98, 96);
+            this.tbxEndereco.Name = "tbxEndereco";
+            this.tbxEndereco.Size = new System.Drawing.Size(330, 34);
+            this.tbxEndereco.TabIndex = 3;
+            // 
+            // tbxDe
+            // 
+            this.tbxDe.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbxDe.Location = new System.Drawing.Point(98, 170);
+            this.tbxDe.Name = "tbxDe";
+            this.tbxDe.Size = new System.Drawing.Size(211, 34);
+            this.tbxDe.TabIndex = 4;
+            this.tbxDe.TextChanged += new System.EventHandler(this.tbxDe_TextChanged);
+            // 
+            // tbxPara
+            // 
+            this.tbxPara.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbxPara.Location = new System.Drawing.Point(98, 233);
+            this.tbxPara.Name = "tbxPara";
+            this.tbxPara.Size = new System.Drawing.Size(211, 34);
+            this.tbxPara.TabIndex = 5;
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.Location = new System.Drawing.Point(390, 233);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(112, 48);
+            this.btnTrocar.TabIndex = 6;
+            this.btnTrocar.Text = "Trocar";
+            this.btnTrocar.UseVisualStyleBackColor = true;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(453, 96);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(49, 28);
+            this.btnPesquisar.TabIndex = 7;
+            this.btnPesquisar.Text = "...";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(611, 376);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.tbxPara);
+            this.Controls.Add(this.tbxDe);
+            this.Controls.Add(this.tbxEndereco);
+            this.Controls.Add(this.lblPara);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.lblEndereco);
+            this.MaximizeBox = false;
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Sistema de troca de arquivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblPara;
+        private System.Windows.Forms.TextBox tbxEndereco;
+        private System.Windows.Forms.TextBox tbxDe;
+        private System.Windows.Forms.TextBox tbxPara;
+        private System.Windows.Forms.Button btnTrocar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ TrocaNomeArquivo/View/TelaPrincipal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaNomeArquivo.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            tbxEndereco.Text = string.Empty;
+
+            try
+            {
+                if(folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    tbxEndereco.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(tbxEndereco.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Selecione a pasta para evetivar a troca de arquivos");
+                }
+                if (tbxDe.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Informe o valor procurado para a troca.");
+                }
+                System.IO.FileInfo[] arquivos = new System.IO.DirectoryInfo(tbxEndereco.Text).GetFiles();
+                foreach(var arquivo in arquivos)
+                {
+                    string novoNome = tbxEndereco.Text + "\\" + arquivo.Name.Replace(tbxDe.Text, tbxPara.Text);
+                    if (!System.IO.File.Exists(novoNome))
+                    {
+                        arquivo.CopyTo(novoNome);
+                        arquivo.Delete();
+                    }
+                }
+                MessageBox.Show("Troca de nome de arquivo efetuada com sucesso...","Aviso",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbxDe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
